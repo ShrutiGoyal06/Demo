15b3547170138750015b0ff686db2f8c5a6cef19
ShrutiGoyal06 <50906475+ShrutiGoyal06@users.noreply.github.com>
2020-03-03 16:11:56 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,4 @@ ls
 
 sudo yum install httpd -y,
 sudo service httpd start,
-sudo chkconfig httpd on,
-echo "Provisioned Infra using Jenkins, Packer and Terraform" > /var/www/html/index.html
+sudo chkconfig httpd on
