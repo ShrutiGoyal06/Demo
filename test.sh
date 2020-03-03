42d0392659454c81ca9629a92f03e00168b4a798
ShrutiGoyal06 <50906475+ShrutiGoyal06@users.noreply.github.com>
2020-03-03 12:28:22 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@ yum install "https://dl.fedoraproject.org/pub/epel/eprl-release-latest-7.noarch.
 sudo yum install mysql* -y
 ls
 
-sudo yum install -y httpd,
-sudo chkconfig httpd on,
+sudo yum install httpd -y,
 sudo service httpd start,
+sudo chkconfig httpd on,
 sudo chown -R ec2-user:ec2-user /var/www,
 echo '<h1> Provisioned infra using Jenkins, Packer and Terraform </h1>' > /var/www/html/index.html
