bee9d3f799eabf243d2897268064bd0172161b7f
ShrutiGoyal06 <50906475+ShrutiGoyal06@users.noreply.github.com>
2020-03-05 12:21:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+ 
+  region   = "us-east-1"
+ 
+}
+ 
+resource "aws_instance" "test" {
+ 
+  ami         = "ami-0d6f3d6526c8dfe7a"
+  instance_type = "t2.micro"
+  key_name = "MyKP"
+  security_groups = ["My SG"]
+ 
+}
