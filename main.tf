807414bf4da9653ac2cbdd3852bca9c8f867dba6
ShrutiGoyal06 <50906475+ShrutiGoyal06@users.noreply.github.com>
2020-03-06 10:16:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
  
   region   = "us-east-1"
   version = "~> 2.50"
-  shared_credentials_file = "/.aws/credentials"
+  shared_credentials_file = "/root/.aws/credentials"
   profile = "default"
   
 }
