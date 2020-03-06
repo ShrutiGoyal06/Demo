e4c3ebf509cabdc3275a4b8f8e871b81a18e5e99
ShrutiGoyal06 <50906475+ShrutiGoyal06@users.noreply.github.com>
2020-03-06 10:08:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
 provider "aws" {
  
-  region   = "us-east-1"
+ version = "~> 2.50"
+ region   = "us-east-1"
+ shared_credentials_file = ".aws/credentials"
+ profile = "default"
+ 
+ 
  
 }
  
 resource "aws_instance" "demo" {
  
-  ami         = "ami-069132f4054c3ef8b"
+  ami         = "ami-02ab9f1c34f810a84"
   instance_type = "t2.micro"
   key_name = "MyKP"
   security_groups = ["My SG"]
